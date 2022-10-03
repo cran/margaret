a853e79ec2e19513406e071b725ec8b9334d00dd
Bryan Arias <bryan.ariasqu@amigo.edu.co>
2022-10-03 20:50:02 +0000

    version 0.1.4

--- R/data.R
+++ R/data.R
@@ -1,21 +1,16 @@
 #Data----------------
 
-#' Scimago 2020 data
+#' "Publindex homologación de revistas" 2003 to 2022
+#' @format A dataframe with information about journal ranking from Publindex
+#' @source \url{https://scienti.minciencias.gov.co/publindex/#/revistasHomologadas/buscador}
+"homologadas_publindex"
+
+#' Publindex Nacional de revistas 2022
+#' @format A dataframe with information about journal ranking from Publindex
+#' @source \url{https://scienti.minciencias.gov.co/publindex/#/revistasPublindex/historico}
+"national_publindex"
+
+#' Scimago categories
 #' @format A dataframe with information about journal ranking from scimago
 #' @source \url{https://github.com/ikashnitsky/sjrdata} \url{https://www.scimagojr.com/}
-"scimago_2020"
-
-#' Scimago 2016-2019 data
-#' @format A dataframe with information about journal ranking from scimago since 2016 to 2019
-#' @source \url{https://github.com/ikashnitsky/sjrdata} \url{https://www.scimagojr.com/}
-"scimago_data"
-
-#' Publindex homologación de revistas 2016 to 2020
-#' @format A dataframe with information about journal ranking from scimago
-#' @source \url{https://scienti.minciencias.gov.co/publindex/#/revistasHomologadas/buscador}
-"international_journals_2016_2020"
-
-#' Publindex homologación de revistas 2021
-#' @format A dataframe with information about journal ranking from scimago
-#' @source \url{https://scienti.minciencias.gov.co/publindex/#/revistasHomologadas/buscador}
-"internationals_journal_2021"
+"scimago_categories"
